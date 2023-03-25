031bf304755a8f985a0d087aef57185b5bcd0bcd
Sn4kk <sankova.ann@gmail.com>
2023-03-26 02:57:48 +0900

    Create project

---
+++ WantedSpaceClone/WantedSpaceClone/WantedSpaceCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WantedSpaceCloneApp.swift
+//  WantedSpaceClone
+//
+//  Created by Hanna Sankova on 2023/03/26.
+//
+
+import SwiftUI
+
+@main
+struct WantedSpaceCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

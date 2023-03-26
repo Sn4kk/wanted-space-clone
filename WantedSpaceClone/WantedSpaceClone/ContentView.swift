d82a64443c319f96b136ace2a9435b44437f682c
Sn4kk <sankova.ann@gmail.com>
2023-03-26 15:20:06 +0900

    Create PaymentsList
    Created PaymentsList View and added it to the Content View

--- WantedSpaceClone/WantedSpaceClone/ContentView.swift
+++ WantedSpaceClone/WantedSpaceClone/ContentView.swift
@@ -20,6 +20,7 @@ struct ContentView: View {
             Divider()
             TodaysNotifications()
             Divider()
+            PaymentsList()
         }
     }
 }
---
+++ WantedSpaceClone/WantedSpaceClone/PaymentsList.swift
@@ -0,0 +1,57 @@
+//
+//  PaymentsList.swift
+//  WantedSpaceClone
+//
+//  Created by Hanna Sankova on 2023/03/26.
+//
+
+import SwiftUI
+
+struct PaymentsList: View {
+    var body: some View {
+        VStack {
+            HStack{
+                Text("결제 항목")
+                    .font(.title3)
+                Spacer()
+                Text("더보기")
+                    .fontWeight(.light)
+            }
+            .padding(30)
+            VStack {
+                ZStack(alignment: .bottom) {
+                    Image(systemName: "doc.plaintext.fill")
+                        .rotationEffect(Angle(degrees: 30), anchor: UnitPoint(x: 0.75, y: 1.25))
+                        .opacity(0.7)
+                        .font(.title)
+                    Image(systemName: "checkmark.rectangle.portrait.fill")
+                        .background(RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.white)
+                            .frame(width: 25, height: 30))
+                        .font(.largeTitle)
+                    
+                }
+                .foregroundColor(Color(UIColor.systemGray4))
+                HStack {
+                    Text("오늘의 결제항목이 없습니다")
+                        .foregroundColor(Color(UIColor.systemGray4))
+                    Image(systemName: "wand.and.stars")
+                        .foregroundColor(.yellow)
+                        .font(.caption)
+                }
+            }
+            .padding(30)
+            .background(Color(UIColor.systemGray6).opacity(0.5))
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+        
+    }
+        
+}
+
+struct PaymentsList_Previews: PreviewProvider {
+    static var previews: some View {
+        PaymentsList()
+    }
+}

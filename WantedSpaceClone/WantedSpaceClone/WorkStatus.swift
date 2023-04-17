922986a2a0048fb6d55df21401840d732dbd60b9
Sn4kk <sankova.ann@gmail.com>
2023-04-18 00:01:40 +0900

    Refactoring with ForEach
    Refactoring with ForEach

--- WantedSpaceClone/WantedSpaceClone/WorkStatus.swift
+++ WantedSpaceClone/WantedSpaceClone/WorkStatus.swift
@@ -22,153 +22,150 @@ struct WorkStatus: View {
                     .foregroundColor(Color(UIColor.darkGray))
                 
             }
-            .padding(30)
+            //            .padding(30)
+            
             
             HStack{
-                
-                VStack {
+                ForEach(userData) { user in
+                    VStack {
+                        Text(user.name)
+                            .font(.title3)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .frame(minWidth: 50, minHeight: 50)
+                            .background(
+                                Circle()
+                                    .fill(user.userIsOnline ? Color(UIColor.systemGray2) :  Color(UIColor.systemGray4))
+                                    .overlay(alignment: .topTrailing) {
+                                        Circle()
+                                            .frame(height: 10)
+                                            .foregroundColor(user.userIsOnline ? Color.green : Color(UIColor.systemGray5))
+                                            .overlay(
+                                                Circle()
+                                                    .stroke(lineWidth: 3)
+                                                    .foregroundColor(.white))
+                                    }
+                            )
+                        Text(user.fullName)
+                            .fontWeight(.light)
+                            .foregroundColor(user.userIsOnline ? Color.black : Color.gray)
+                    }
                     
-                    Text("나(")
-                        .font(.title3)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .frame(maxWidth: 50, maxHeight: 50)
+                    Spacer()
                     
-                        .background(
-                            Circle()
-                                .fill(userIsOnline ? Color(UIColor.systemGray2) :  Color(UIColor.systemGray4))
-//                                .frame(height: 50)
-                                .overlay(alignment: .topTrailing) {
-                                    Circle()
-                                        .frame(height: 10)
-                                        .foregroundColor(userIsOnline ? Color.green : Color(UIColor.systemGray5))
-                                        .overlay(
-                                            Circle()
-                                                .stroke(lineWidth: 3)
-                                                .foregroundColor(.white))
-                                }
-                            
-                        )
-                    
-                        .padding(.bottom)
-                    
-                    Text("한나(나)")
-                        .fontWeight(.light)
-                        .foregroundColor(.gray)
-                }
-                Spacer()
-                
-                VStack {
-                    
-                    Text("길동") .font(.title3)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .frame(maxWidth: 50, maxHeight: 50)
-                    
-                        .background(
-                            Circle()
-                                .fill(Color(UIColor.systemGray4))
-                                .frame(height: 50)
-                                .overlay(alignment: .topTrailing) {
-                                    Circle()
-                                        .frame(height: 10)
-                                        .foregroundColor(Color(UIColor.systemGray5))
-                                        .overlay(
-                                            Circle()
-                                                .stroke(lineWidth: 3)
-                                                .foregroundColor(.white))
-                                }
-                            
-                        )
-                    
-                        .padding(.bottom)
-                    
-                    Text("홍길동")
-                        .fontWeight(.light)
-                        .foregroundColor(.gray)
-                }
-                Spacer()
-                
-                VStack {
-                    
-                    Text("길동")
-                        .font(.title3)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .frame(maxWidth: 50, maxHeight: 50)
-                    
-                        .background(
-                            Circle()
-                                .fill(Color(UIColor.systemGray4))
-                                .frame(height: 50)
-                                .overlay(alignment: .topTrailing) {
-                                    Circle()
-                                        .frame(height: 10)
-                                        .foregroundColor(Color(UIColor.systemGray5))
-                                        .overlay(
-                                            Circle()
-                                                .stroke(lineWidth: 3)
-                                                .foregroundColor(.white))
-                                }
-                            
-                        )
-                        .padding(.bottom)
-                    
-                    Text("홍길동")
-                        .fontWeight(.light)
-                        .foregroundColor(.gray)
-                }
-                Spacer()
-                
-                VStack {
-                    
-                    Text("길동")
-                        .font(.title3)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .frame(maxWidth: 50, maxHeight: 50)
-                    
-                        .background(
-                            Circle()
-                                .fill(Color(UIColor.systemGray4))
-                                .frame(height: 50)
-                                .overlay(alignment: .topTrailing) {
-                                    Circle()
-                                        .frame(height: 10)
-                                        .foregroundColor(Color(UIColor.systemGray5))
-                                        .overlay(
-                                            Circle()
-                                                .stroke(lineWidth: 3)
-                                                .foregroundColor(.white))
-                                }
-                        )
-                        .padding(.bottom)
-                    
-                    Text("홍길동")
-                        .fontWeight(.light)
-                        .foregroundColor(.gray)
                 }
             }
-            .padding(.horizontal, 30)
-            .padding(.trailing,50)
+            
         }
     }
 }
-
 struct WorkStatus_Previews: PreviewProvider {
     static var previews: some View {
         WorkStatus()
     }
 }
 
-//struct user {
-//    var userIsOnline: Bool
-//    let name: String
-//}
+struct User: Identifiable {
+    let id = UUID()
+    var userIsOnline: Bool
+    let name: String
+    let fullName: String
+}
+
+let userData : [User] = [
+    User(userIsOnline: true, name: "나", fullName: "한나"),
+    User(userIsOnline: false, name: "길동", fullName: "홍길동"),
+    User(userIsOnline: false, name: "길동", fullName: "홍길동"),
+    User(userIsOnline: true, name: "길동", fullName: "홍길동"),
+]
+
+//                    VStack {
 //
-//let userData : [user] = [
-//    user(userIsOnline: true, name: "나"),
-//    user(userIsOnline: false, name: "홍길동"),
-//    user(userIsOnline: false, name: "홍길동"),
-//    user(userIsOnline: false, name: "홍길동"),
-//]
+//                        Text("길동") .font(.title3)
+//                            .fontWeight(.bold)
+//                            .foregroundColor(.white)
+//                            .frame(maxWidth: 50, maxHeight: 50)
+//
+//                            .background(
+//                                Circle()
+//                                    .fill(Color(UIColor.systemGray4))
+//                                    .frame(height: 50)
+//                                    .overlay(alignment: .topTrailing) {
+//                                        Circle()
+//                                            .frame(height: 10)
+//                                            .foregroundColor(Color(UIColor.systemGray5))
+//                                            .overlay(
+//                                                Circle()
+//                                                    .stroke(lineWidth: 3)
+//                                                    .foregroundColor(.white))
+//                                    }
+//
+//                            )
+//
+//                            .padding(.bottom)
+//
+//                        Text("홍길동")
+//                            .fontWeight(.light)
+//                            .foregroundColor(.gray)
+//                    }
+//                    Spacer()
+//
+//                    VStack {
+//
+//                        Text("길동")
+//                            .font(.title3)
+//                            .fontWeight(.bold)
+//                            .foregroundColor(.white)
+//                            .frame(maxWidth: 50, maxHeight: 50)
+//
+//                            .background(
+//                                Circle()
+//                                    .fill(Color(UIColor.systemGray4))
+//                                    .frame(height: 50)
+//                                    .overlay(alignment: .topTrailing) {
+//                                        Circle()
+//                                            .frame(height: 10)
+//                                            .foregroundColor(Color(UIColor.systemGray5))
+//                                            .overlay(
+//                                                Circle()
+//                                                    .stroke(lineWidth: 3)
+//                                                    .foregroundColor(.white))
+//                                    }
+//
+//                            )
+//                            .padding(.bottom)
+//
+//                        Text("홍길동")
+//                            .fontWeight(.light)
+//                            .foregroundColor(.gray)
+//                    }
+//                    Spacer()
+//
+//                    VStack {
+//
+//                        Text("길동")
+//                            .font(.title3)
+//                            .fontWeight(.bold)
+//                            .foregroundColor(.white)
+//                            .frame(maxWidth: 50, maxHeight: 50)
+//
+//                            .background(
+//                                Circle()
+//                                    .fill(Color(UIColor.systemGray4))
+//                                    .frame(height: 50)
+//                                    .overlay(alignment: .topTrailing) {
+//                                        Circle()
+//                                            .frame(height: 10)
+//                                            .foregroundColor(Color(UIColor.systemGray5))
+//                                            .overlay(
+//                                                Circle()
+//                                                    .stroke(lineWidth: 3)
+//                                                    .foregroundColor(.white))
+//                                    }
+//                            )
+//                            .padding(.bottom)
+//
+//                        Text("홍길동")
+//                            .fontWeight(.light)
+//                            .foregroundColor(.gray)
+//                    }
